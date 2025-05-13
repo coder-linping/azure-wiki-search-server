32c6c88984a12edf31a6b97b96f28422b444dd7b
Linping Zhang <linpzhan@microsoft.com>
2025-05-13 12:51:15 +0800

    Create azure wiki search server

---
+++ README.md
@@ -0,0 +1,71 @@
+# Azure Wiki Search Server
+This mcp server implements the MCP specification to allow AI agents to search on Azure wiki.
+
+## Tools
+* __search_wiki__
+
+    Search Edge Wiki to find related material for {query}.
+* __get_wiki_by_path__
+
+    Get wiki content by provided path.
+
+## Prerequest 
+1. Install the [latest VS code](https://code.visualstudio.com/download).
+2. Install the [GitHub Copilot](https://marketplace.visualstudio.com/items?itemName=GitHub.copilot) and [GitHub Copilot Chat](https://marketplace.visualstudio.com/items?itemName=GitHub.copilot-chat) extensions
+3. Install Python 3.10 or higher.
+4. Install uv.
+    On Windows
+    ```
+    powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+    ```
+
+    On Mac|Linux
+    ```
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    ```
+5. Prepare a Personal Access Token and make sure it's have permission to read wiki.
+
+## Local Setup
+1. Clone this repo.
+    ```
+    git clone https://github.com/coder-linping/azure-wiki-search-server.git 
+    cd azure-wiki-search-server
+    ```
+2. Setup env.
+    On Windows
+    ```
+    uv venv
+    .venv/Script/activate
+    ```
+
+    On Mac | Linux
+    ```
+    uv venv
+    source .venv/bin/activate
+    ```
+3. Configuration for VS Code
+
+    For manual installation, add the following JSON block to your User Settings (JSON) file in VS Code. You can do this by pressing Ctrl + Shift + P and typing Preferences: Open User Settings (JSON).
+
+    Optionally, you can add it to a file called .vscode/mcp.json in your workspace. This will allow you to share the configuration with others.
+
+    ```
+      "mcp": {
+        "servers": {
+          "edge_wiki": {
+            "command": "uv",
+            "args": [
+                "--directory",
+                "<absolute path to your cloned folder>",
+                "run",
+                "src/edge_wiki.py"
+            ],
+            "env": {
+                "PAT": "Your personal access token",
+                "ORG": "Your organization，default is microsoft",
+                "PROJECT": "Your project, default is Edge"
+            },
+          }
+        }
+      }
+    ```
---
+++ src/edge_wiki.py
@@ -0,0 +1,79 @@
+import json
+import os
+from azure.devops.connection import Connection
+from azure.devops.v7_0.search.models import WikiSearchRequest 
+from msrest.authentication import BasicAuthentication
+from mcp.server.fastmcp import FastMCP
+from urllib.parse import unquote
+
+mcp = FastMCP("edge_wiki")
+
+org = os.getenv('ORG', 'microsoft')
+project = os.getenv('PROJECT', 'Edge')
+
+def get_connection() -> Connection:
+    # Get PAT from environment variable
+    personal_access_token = os.getenv('PAT')
+    if not personal_access_token:
+        raise ValueError("PAT environment variable is not set")
+        
+    organization_url = f'https://dev.azure.com/{org}'
+    credentials = BasicAuthentication('', personal_access_token)
+    connection = Connection(base_url=organization_url, creds=credentials)
+    return connection
+
+@mcp.prompt()
+def start_edge_wiki_search() -> str:
+    """ Prompt user for a search query to find Edge Wiki content"""
+
+    return """
+      You're an AI assistant for Microsoft Edge Wiki.
+
+      Your task is to analyze the user's question and extract the relevant keywords to form a query. Ensure that words like "How to," "What is," and similar phrases are removed from the query.
+      If the query does not works, please try simpifying the query and try again.
+
+      Use this refined query to search for the relevant wiki entries using `search_wiki`.
+
+      Once you obtain the wiki path from the search results, use it as is to retrieve the full wiki content using search_wiki_by_path. It is important to note that the path should not be decoded when it is URL-encoded.
+
+      Answer the user's question based on the comprehensive information found in the wiki content.
+    """
+
+@mcp.tool()
+async def search_wiki(query: str) -> str:
+    """ Search Edge Wiki to find related material for {query}"""
+    
+    try:
+        search_client = get_connection().clients.get_search_client()
+
+        search_request = WikiSearchRequest(search_text=query, top=500)
+        result = search_client.fetch_wiki_search_results(request = search_request, project=project)
+
+        return json.dumps([{
+            'file_name': wiki.file_name,
+            'path': wiki.path 
+        } for wiki in result.results], indent=2)
+
+    except Exception as e:
+        return f"Error searching wiki: {str(e)}"  
+
+@mcp.tool()
+async def get_wiki_by_path(path: str) -> str:
+    """ Get Edge Wiki content by path returned from search_wiki"""
+
+    wiki_client = get_connection().clients.get_wiki_client()
+
+    # replace - with space and remove .md extension
+    correct_path = path.replace("-", " ").replace(".md", "")
+    correct_path = unquote(correct_path)
+    result = wiki_client.get_page(project='Edge', wiki_identifier='Edge.wiki', path=correct_path, include_content=True)
+
+    return json.dumps({
+            'path': result.page.path,
+            'content': result.page.content
+    }, indent=2)
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    mcp.run(transport='stdio')
+
